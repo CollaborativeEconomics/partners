1e418777e45b517ac4dfb44793074865febf19db
Julian Kingman <julian.kingman@gmail.com>
2024-08-31 12:04:31 -0500

    fix factory address

--- pages/dashboard/events/[id].tsx
+++ pages/dashboard/events/[id].tsx
@@ -14,11 +14,7 @@ import { getEventById } from 'utils/registry';
 import styles from 'styles/dashboard.module.css';
 import ButtonBlue from 'components/buttonblue';
 import LinkButton from 'components/linkbutton';
-import {
-  useConnect,
-  useAccount,
-  useWriteContract,
-} from 'wagmi';
+import { useConnect, useAccount, useWriteContract } from 'wagmi';
 import { readContract, switchChain, waitForTransaction } from '@wagmi/core';
 import { arbitrumSepolia } from 'wagmi/chains';
 import { config } from 'chains/config';
@@ -108,7 +104,7 @@ export default function Event({ id, event, media, contractNFT, contractV2E }) {
   }
 
   // TODO: move to config file
-  const FactoryAddress = '0xA14F3dD410021c7f05Ca1aEf7aDc9C86943E839f';
+  const FactoryAddress = '0xD4E47912a12f506843F522Ea58eA31Fd313eB2Ee';
   const usdcAddressTestnet = '0x75faf114eafb1BDbe2F0316DF893fd58CE46AA4d';
   let NFTBlockNumber: number;
   let distributorBlockNumber: number;
